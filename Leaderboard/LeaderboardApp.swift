2310d1e89e4ef01cb3d7d93e4909b32990b548a2
Mykyta Babanin <shroednick@gmail.com>
2023-06-17 09:06:24 +0100

    Initial Commit

---
+++ Leaderboard/LeaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaderboardApp.swift
+//  Leaderboard
+//
+//  Created by Mykyta Babanin on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct LeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

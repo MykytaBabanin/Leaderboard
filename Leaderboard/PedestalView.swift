2c5a9ed2a7b18b21fbe43184aaf345286dc48f48
Mykyta Babanin <shroednick@gmail.com>
2023-06-18 16:40:54 +0100

    Created Leadearboard application using Copilot and ChatGPT

---
+++ Leaderboard/PedestalView.swift
@@ -0,0 +1,18 @@
+//
+//  PedestalView.swift
+//  Leaderboard
+//
+//  Created by Mykyta Babanin on 18.06.2023.
+//
+
+import SwiftUI
+
+struct PedestalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PedestalView()
+}
---
+++ Leaderboard/RegionModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegionModel.swift
+//  Leaderboard
+//
+//  Created by Mykyta Babanin on 18.06.2023.
+//
+
+import Foundation
